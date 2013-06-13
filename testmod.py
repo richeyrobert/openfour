010b722fd922cad60ceab7a872fd18ec1c5d7ce9
Robert Richey <rob@rocketnext.com>
2013-06-12 22:54:57 -0600

    Adds missing testmod.py file. This is the module that is run remotely.

---
+++ testmod.py
@@ -0,0 +1,5 @@
+class testclass:
+    def mul(s, arg1, arg2): return arg1*arg2
+    def add(s, arg1, arg2): return arg1+arg2
+    def sub(s, arg1, arg2): return arg1-arg2
+    def div(s, arg1, arg2): return arg1/arg2
